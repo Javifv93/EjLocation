6039812dfc45f93b3e40150b1a273db5c324fb54
javifv <javifervaz.com>
2021-02-04 14:27:55 +0100

    Copiando código de los apuntes, página 2

--- app/src/main/java/com/example/ejlocation/MainActivity.java
+++ app/src/main/java/com/example/ejlocation/MainActivity.java
@@ -1,14 +1,41 @@
 package com.example.ejlocation;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private Location localizacion;
+    private LocationManager localizador;            // TODO: 04/02/2021 Hay que instanciarlos
+    private boolean hay_conexion_gps;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Pide permisos al usuario de acceso al gps
+        ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
+
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                !=
+                PackageManager.PERMISSION_GRANTED){
+                    //Estos son elementos del activity, hay que definirlos aun
+                    //latitud.setText("No se han definido los permisos necesarios");
+                    //longitud.setText("");
+                    return;
+                }
+
+        // TODO: 04/02/2021 Peta por usar un metodo de un objeto nulo (localizador, que esta sin instanciar)
+        //true/false si el gps esta activado
+        hay_conexion_gps = localizador.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+
+
     }
 }
