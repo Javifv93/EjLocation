937e15b9bf249a369c7f3ea46f6228d057e9685e
javifv <javifervaz.com>
2021-02-11 13:55:31 +0100

    Acabada!

--- app/src/main/java/com/example/ejlocation/MainActivity.java
+++ app/src/main/java/com/example/ejlocation/MainActivity.java
@@ -7,28 +7,50 @@ import androidx.core.app.ActivityCompat;
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements LocationListener{
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity implements LocationListener, OnMapReadyCallback {
     private Location localizacion;
     private LocationManager location_manager;
-    private boolean hay_conexion_gps;
 
     private TextView tv_longitud;
     private TextView tv_latitud;
     private TextView tv_altitud;
     private TextView tv_precision;
 
+    private static final String MAP_VIEW_BUNDLE_KEY = "MapViewBundleKey";
+    private MapView vistaMapa;
+    private GoogleMap gmap;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Bundle mapViewBundle = null;
+        if (savedInstanceState != null) {
+            mapViewBundle = savedInstanceState.getBundle(MAP_VIEW_BUNDLE_KEY);
+        }
 
         //Pide permisos al usuario de acceso al gps
         ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
@@ -36,8 +58,79 @@ public class MainActivity extends AppCompatActivity implements LocationListener{
         tv_latitud = (TextView) findViewById(R.id.str_latitud);
         tv_altitud = (TextView) findViewById(R.id.str_altitud);
         tv_precision = (TextView) findViewById(R.id.stra_precision);
+
         getLocation();
+
+        vistaMapa = findViewById(R.id.map);
+        vistaMapa.onCreate(mapViewBundle);
+        vistaMapa.getMapAsync(this);
     }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        Bundle mapViewBundle = outState.getBundle(MAP_VIEW_BUNDLE_KEY);
+        if (mapViewBundle == null) {
+            mapViewBundle = new Bundle();
+            outState.putBundle(MAP_VIEW_BUNDLE_KEY, mapViewBundle);
+        }
+        vistaMapa.onSaveInstanceState(mapViewBundle);
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        vistaMapa.onResume();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        vistaMapa.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        vistaMapa.onStop();
+    }
+    @Override
+    protected void onPause() {
+        vistaMapa.onPause();
+        super.onPause();
+    }
+    @Override
+    protected void onDestroy() {
+        vistaMapa.onDestroy();
+        super.onDestroy();
+    }
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        vistaMapa.onLowMemory();
+    }
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        gmap = googleMap;
+        gmap.setMinZoomPreference(12);
+        LatLng ny = new LatLng(40.7143528, -74.0059731);
+        gmap.moveCamera(CameraUpdateFactory.newLatLng(ny));
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(0,0)).title("Marker"));
+
+        gmap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                //Añade un marcador al mapa
+                //&gmap.addMarker(new MarkerOptions()
+                   //     .position(latLng));
+                tv_longitud.setText(getResources().getString(R.string.str_longitud) + " " + latLng.longitude);
+                tv_latitud.setText(getResources().getString(R.string.str_latitud) + " " + latLng.latitude);
+                //gmap.clear();
+            }
+        });
+    }
+
+    //LOCATION
     @SuppressLint("ResourceType")
     public void getLocation(){
         location_manager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
@@ -79,4 +172,5 @@ public class MainActivity extends AppCompatActivity implements LocationListener{
     public void onProviderEnabled(@NonNull String provider) {
 
     }
+
 }
